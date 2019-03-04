7f5acc4709a3c956a10997c069663f0ad3346521
Bexxkie <bkg360@hotmail.com>
2019-03-03 22:01:16 -0700

    enchantment command requires 5x5 of air around player to use

--- src/main/java/com/bex/bexPack/commands/EnchantTableCommand.java
+++ src/main/java/com/bex/bexPack/commands/EnchantTableCommand.java
@@ -45,54 +45,49 @@ public class EnchantTableCommand
 								.of(InventoryArchetypes.ENCHANTING_TABLE)
 								.build(PixelCandy.INSTANCE);
 
-						
+
 						Location loc = p.getLocation();
 						HashMap<Location,BlockType> tmp = new HashMap<Location,BlockType>();
 						for(int x = 0;x<2;x++)
 						{
 							tmp.put(loc.add(2, x, 0), loc.add(2, x, 0).getBlockType());
-							loc.add(2, x, 0).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(2, x, 1), loc.add(2, x, 1).getBlockType());
-							loc.add(2, x, 1).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(2, x, -1), loc.add(2, x, -1).getBlockType());
-							loc.add(2, x, -1).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(-2, x, 0), loc.add(-2, x, 0).getBlockType());
-							loc.add(-2, x, 0).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(-2, x, 1), loc.add(-2, x, 1).getBlockType());
-							loc.add(-2, x, 1).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(-2, x, -1), loc.add(-2, x, -1).getBlockType());
-							loc.add(-2, x, -1).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(0, x, -2), loc.add(0, x, -2).getBlockType());
-							loc.add(0, x, -2).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(-1, x, -2), loc.add(-1, x, -2).getBlockType());
-							loc.add(-1, x, -2).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(1, x, -2), loc.add(1, x, -2).getBlockType());
-							loc.add(1, x, -2).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(0, x, 2), loc.add(0, x, 2).getBlockType());
-							loc.add(0, x, 2).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(-1, x, 2), loc.add(-1, x, 2).getBlockType());
-							loc.add(-1, x, 2).setBlockType(BlockTypes.BOOKSHELF);
 							tmp.put(loc.add(1, x, 2), loc.add(1, x, 2).getBlockType());
-							loc.add(1, x, 2).setBlockType(BlockTypes.BOOKSHELF);
-							
-							tmp.put(loc.add(1, x, 0), loc.add(1, x, 0).getBlockType());
-							loc.add(1, x, 0).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(1, x, 1), loc.add(1, x, 1).getBlockType());
-							loc.add(1, x, 1).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(1, x, -1), loc.add(1, x, -1).getBlockType());
-							loc.add(1, x, -1).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(-1, x, 0), loc.add(-1, x, 0).getBlockType());
-							loc.add(-1, x, 0).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(-1, x, 1), loc.add(-1, x, 1).getBlockType());
-							loc.add(-1, x, 1).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(-1, x, -1), loc.add(-1, x, -1).getBlockType());
-							loc.add(-1, x, -1).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(0, x, -1), loc.add(0, x, -1).getBlockType());
-							loc.add(0, x, -1).setBlockType(BlockTypes.AIR);
-							tmp.put(loc.add(0, x, 1), loc.add(0, x, 1).getBlockType());
-							loc.add(0, x, 1).setBlockType(BlockTypes.AIR);
-							
+
 						}
+						for(Location l : tmp.keySet())
+						{
+							if(tmp.get(l).equals(BlockTypes.AIR))
+							{
+								src.sendMessage(Text.of("[bPack] Not enough room"));
+								return CommandResult.success();
+							}
+						}
+						for(int x = 0;x<2;x++) 
+						{
+							loc.add(1, x, 2).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(2, x, 0).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(2, x, 1).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(2, x, -1).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(-2, x, 0).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(-2, x, 1).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(-2, x, -1).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(0, x, -2).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(-1, x, -2).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(1, x, -2).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(0, x, 2).setBlockType(BlockTypes.BOOKSHELF);
+							loc.add(-1, x, 2).setBlockType(BlockTypes.BOOKSHELF);
+						}
+
 						if(PixelCandy.enchantingBlockMap.containsKey(p))
 						{
 							PixelCandy.enchantingBlockMap.remove(p);
@@ -108,5 +103,5 @@ public class EnchantTableCommand
 	public CommandSpec getCommandSpec() {
 		return commandSpec;
 	}
-	
+
 }
