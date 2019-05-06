0387300a18a6e68c223278340ec1553de102aff8
Bexxkie <bkg360@hotmail.com>
2019-05-05 21:47:13 -0600

    updated version number

--- src/main/java/com/bex/bexPack/main/PixelCandy.java
+++ src/main/java/com/bex/bexPack/main/PixelCandy.java
@@ -41,7 +41,7 @@ dependencies = {@Dependency(id = "pixelmon")})
 public class PixelCandy 
 {
 	public static final String NAME = "pixelCandy";
-	public static final String VERSION = "1.2.4h";
+	public static final String VERSION = "1.2.4i";
 	public static final String AUTHOR = "Bexxkie, Dr. Stupid";
 	public static final String DESC = "This does a bunch of shit...";
 	@Inject
